2e0e1933be97a348c6539833e17cda322b3434fb
Branden M <brandenmcneill21@gmail.com>
2022-03-13 23:16:35 -0700

    Milestone 1 (INCOMPLETE)

---
+++ Milestone1/GameBoard.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Milestone1
+{
+    public class GameBoard
+    {
+        public int Size { get; set; } // This will get and set the board size
+
+        public GameCell[,] BoardGrid { get; set; }
+
+        public string Difficulty { get; set; } // This will determine the difficulty for the user 
+        // This will also set how many cells that will be live with bombs for the user to contend with
+
+        public int SetupLiveNeighbors { get; set; }
+
+
+        public GameBoard(int s) // This will set the size of the grid
+        {
+            Size = s;
+
+            BoardGrid = new GameCell[Size, Size]; // Needs 2 Size's for the Row and the Column of the Boards Grid
+
+            for (int i = 0; i < Size; i++)
+            {
+                for (int j = 0; j < Size; j++)
+                {
+                    BoardGrid[i, j] = new GameCell();
+                }
+            }
+        }
+        public string userDifficulty()
+        {
+            return Difficulty;
+        }
+
+        /* INCOMPLETE */
+
+        public setupLiveNeighbors()
+        {
+            for (int i = 0; i < Size; i++)
+            {
+                for (int j = 0; j < Size; j++)
+                {
+                    BoardGrid[i, j].number_of_neighbors++;
+                };
+            }
+        }
+    
+        public 
+
+
---
+++ Milestone1/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Milestone1
+{
+    public class Program
+    {
+        //Here we will setup the GameBoard for the user
+        static GameBoard gameBoard = new GameBoard(16); // Wanted a larger game board when playing minesweeper
+
+        public static string Difficulty { get; private set; }
+
+        static void Main(string[] args)
+        {
+            // Now we will print a new minesweeper game board.
+            printGameBoard(gameBoard);
+
+            GameCell users_cell = Set_Users_Cell();
+            users_cell.ItsVisible = true;
+        }
+
+        private static GameCell Set_Users_Cell()
+        {
+            Console.WriteLine("What row would you like to start in?");
+            int Row = int.Parse(Console.ReadLine());
+            // This will set the row for the user to start in.
+
+            Console.WriteLine("What column would you like to start in?");
+            int Column = int.Parse(Console.ReadLine());
+            // This will set the column for the user to start in.
+
+            gameBoard.BoardGrid[Row, Column].ItsVisible = true;
+        }
+
+        private static void printGameBoard(GameBoard gameBoard)
+        {
+
+            // This is going to give us a visual of the game board layout
+            for (int i = 0; i < gameBoard.Size; i++)
+            {
+                for (int a = 0; a < gameBoard.Size; a++)
+                {
+                    Console.WriteLine("+--");
+                }
+                Console.Write("+");
+                Console.Write("");
+                Console.Write("|");
+
+                for (int j = 0 ; j < gameBoard.Size; j++)
+                {
+                    GameCell gc = gameBoard.BoardGrid[i,j];
+
+                    if (gc.Live == true)
+                    {
+                        Console.WriteLine("What would you like the difficulty to be?");
+                        Console.WriteLine("1 for Easy, 2 for Medium, 3 for Hard");
+                        Difficulty = Console.ReadLine();
+                        if (Difficulty == "1")
+                        {
+                            Console.WriteLine("Difficulty is set to Easy.");
+                        }
+                        if (Difficulty == "2")
+                        {
+                            Console.WriteLine("Difficulty is set to Medium.");
+                        }
+
+                        if (Difficulty == "3")
+                        {
+                            Console.WriteLine("Difficulty is set to Hard!");
+                        }
+                        break;
+                    }
+                    Console.WriteLine("-----------------------------------------");
+                }   
+            }
+        }
+    }
+}
