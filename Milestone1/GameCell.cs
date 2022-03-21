e6d6984133901d81dbae064bd085f68c4f6ead4d
Branden M <brandenmcneill21@gmail.com>
2022-03-20 20:50:39 -0700

    Milestone 1

---
+++ Milestone1/GameCell.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Milestone1
+{
+    public class GameCell
+    {
+        public int Row { get; set; }
+        public int Column { get; set; }
+        public bool ItsVisible { get; set; }
+
+        public string users_cell { get; set; }
+        
+        public bool Live { get; set; }
+
+        public int number_of_neighbors { get; set; }
+    }
+
+    public GameCell(int r, int c)
+    {
+        int Row = r;
+        int Column = c;
+    }
+}
+
